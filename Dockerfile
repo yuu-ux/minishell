855f0aef55d50e477671b09dd9fd49d531e23b8b
yuu-ux <yuugaehara@gmail.com>
2024-12-02 13:28:55 +0000

    :recycle: githook のテストとついでの変更
    - Docker インストールするもの追加
    - main の norminette 修正

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
+# syntax=docker/dockerfile:1
+
 FROM --platform=linux/x86_64 ubuntu:20.04
 
-RUN apt update && apt install -y make git gcc curl tmux zsh bear libreadline-dev man-db
+RUN apt update && apt install -y make git gcc curl tmux zsh bear libreadline-dev man-db python3 python3-pip python3-venv ripgrep
 
 SHELL ["/bin/zsh", "-c"]
 
--- srcs/main.c
+++ srcs/main.c
@@ -10,70 +10,71 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <expand.h>
+#include <invoke_commands.h>
+#include <libft.h>
+#include <minishell.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <signal_setting.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <readline/readline.h>
-#include <readline/history.h>
 #include <tokenize.h>
-#include <minishell.h>
-#include <expand.h>
-#include <libft.h>
-#include <invoke_commands.h>
-#include <signal_setting.h>
 
-static kvs *create_path(char **environ)
+static kvs	*create_path(char **environ)
 {
-    kvs *path_list;
-    char **temp;
-    int i;
-    int count;
+	kvs		*path_list;
+	char	**temp;
+	int		i;
+	int		count;
 
-    i = 0;
-    count = 0;
-    while (environ[i])
-    {
-        count++;
-        i++;
-    }
-    path_list = (kvs *)malloc(sizeof(kvs) * (count+1));
-    if (!path_list)
-        return NULL;
-    ft_memset(path_list, 0, sizeof(kvs));
-    i = 0;
-    while (environ[i])
-    {
-        temp = ft_split(environ[i], '=');
-        path_list[i].key = temp[0];
-        path_list[i].value = temp[1];
-        free(temp);
-        i++;
-    }
-    return (path_list);
+	i = 0;
+	count = 0;
+	while (environ[i])
+	{
+		count++;
+		i++;
+	}
+	path_list = (kvs *)malloc(sizeof(kvs) * (count + 1));
+	if (!path_list)
+		return (NULL);
+	ft_memset(path_list, 0, sizeof(kvs));
+	i = 0;
+	while (environ[i])
+	{
+		temp = ft_split(environ[i], '=');
+		path_list[i].key = temp[0];
+		path_list[i].value = temp[1];
+		free(temp);
+		i++;
+	}
+	return (path_list);
 }
 
-int main(void)
+int	main(void)
 {
-    char *line = NULL;
-    t_token *tokens;
-    extern char **environ;
-    kvs *path_list;
+	char		*line;
+	t_token		*tokens;
+	extern char	**environ;
+	kvs			*path_list;
 
-    // unset で消えることもある
-    // environは自動で更新されないため、更新する必要がある
-    path_list = create_path(environ);
-    signal_setting();
-    while (1)
-    {
-        line = readline("minishell$ ");
-        tokens = tokenization(line);
-        check_syntax(tokens);
-        tokens = expand_tokens(&tokens, path_list);
-        invoke_commands(tokens);
-        ////all_free();
-        add_history(line);
-        free(line);
-    }
-    printf("exit\n");
-    return (SUCCESS);
+	line = NULL;
+	// unset で消えることもある
+	// environは自動で更新されないため、更新する必要がある
+	path_list = create_path(environ);
+	signal_setting();
+	while (1)
+	{
+		line = readline("minishell$ ");
+		tokens = tokenization(line);
+		check_syntax(tokens);
+		tokens = expand_tokens(&tokens, path_list);
+		invoke_commands(tokens);
+		////all_free();
+		add_history(line);
+		free(line);
+	}
+	printf("exit\n");
+	return (SUCCESS);
 }
