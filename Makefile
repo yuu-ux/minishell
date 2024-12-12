88c1ba48c2e442dcce68f21c66087c69f9f533b6
yuu-ux <yuugaehara@gmail.com>
2024-12-12 11:31:19 +0000

    :heavy_plus_sign: オブジェクトファイルを生成してからコンパイルするように修正

--- Makefile
+++ Makefile
@@ -2,20 +2,25 @@ NAME = minishell
 
 CC = cc
 CFLAGS = -g -Wall -Wextra -Werror -I./includes/ -Ilibft
-FILES = main.c tokenize.c check_syntax.c utils.c debug.c expand_tokens.c invoke_commands.c signal_setting.c parse.c
-SRCS = $(addprefix ./srcs/, $(FILES))
+SRCS = main.c tokenize.c check_syntax.c utils.c debug.c expand_tokens.c invoke_commands.c signal_setting.c parse.c
+SRC_DIR = ./srcs
+OBJ_DIR = ./objs
+OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o)
 LIBFT = libft
 TEST_DIR = ./test/tokenize/
 
 all: $(NAME)
 
-$(NAME): $(SRCS)
+$(NAME): $(OBJS)
 	make -C $(LIBFT)
 	$(CC) $(CFLAGS) $^ -o $@ ./libft/libft.a -lreadline
 
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
 clean:
 	make -C $(LIBFT) clean
-	rm -f *.o
+	rm -f $(OBJ_DIR)/*.o
 
 fclean: clean
 	make -C $(LIBFT) fclean
--- srcs/invoke_commands.c
+++ srcs/invoke_commands.c
@@ -1,8 +1,8 @@
-#include <invoke_commands.h>
 #include <debug.h>
+#include <invoke_commands.h>
 #include <signal_setting.h>
 
-//int exec_builtin(t_node *parsed_tokens)
+// int exec_builtin(t_node *parsed_tokens)
 //{
 //    if (ft_strncmp(parsed_tokens->argv[0], "echo", 5))
 //        built_echo();
@@ -19,60 +19,64 @@
 //    else if (ft_strncmp(parsed_tokens->argv[0], "exit", 5))
 //        built_exit();
 //    else
-//        return 1;
+//        return (1);
 //}
 
-int exec_cmd(t_node *parsed_tokens, char **path)
+int	exec_cmd(t_node *parsed_tokens, char **path)
 {
-    int i;
+	int		i;
+	char	*temp;
+	char	*res;
 
-    // TODO ビルトイン 作成
-    //exec_builtin(parsed_tokens);
-    if (parsed_tokens->argv[0][0] == '/')
-    {
-        // 絶対パス
-        while (parsed_tokens)
-        {
-            if (access(parsed_tokens->argv[0], F_OK) == 0)
-                execve(parsed_tokens->argv[0], parsed_tokens->argv, NULL);
-            parsed_tokens = parsed_tokens->next;
-        }
-    }
-    else
-    {
-        // 相対パスで渡されたパターン
-        while (parsed_tokens)
-        {
-            i = 0;
-            while (path[i])
-            {
-                char *temp = ft_strjoin(path[i], "/");
-                char *res = ft_strjoin(temp, parsed_tokens->argv[0]);
-                if (access(res, F_OK) == 0)
-                    execve(res, parsed_tokens->argv, NULL);
-                i++;
-            }
-            parsed_tokens = parsed_tokens->next;
-        }
-    }
-    return 1;
+	if (fork() == -1)
+		perror("error\n");
+	// TODO ビルトイン 作成
+	// exec_builtin(parsed_tokens);
+	if (parsed_tokens->argv[0][0] == '/')
+	{
+		// 絶対パス
+		while (parsed_tokens)
+		{
+			if (access(parsed_tokens->argv[0], F_OK) == 0)
+				execve(parsed_tokens->argv[0], parsed_tokens->argv, NULL);
+			parsed_tokens = parsed_tokens->next;
+		}
+	}
+	else
+	{
+		// 相対パスで渡されたパターン
+		while (parsed_tokens)
+		{
+			i = 0;
+			while (path[i])
+			{
+				temp = ft_strjoin(path[i], "/");
+				res = ft_strjoin(temp, parsed_tokens->argv[0]);
+				if (access(res, F_OK) == 0)
+					execve(res, parsed_tokens->argv, NULL);
+				i++;
+			}
+			parsed_tokens = parsed_tokens->next;
+		}
+	}
+	return (1);
 }
 
-char **get_path(char *path)
+char	**get_path(char *path)
 {
-    char **result;
+	char	**result;
 
-    result = ft_split(path, ':');
-    return (result);
+	result = ft_split(path, ':');
+	return (result);
 }
 
-void    invoke_commands(t_token *tokens)
+void	invoke_commands(t_token *tokens)
 {
-    t_node *parsed_tokens;
-    char **path;
+	t_node	*parsed_tokens;
+	char	**path;
 
-    path = get_path(getenv("PATH"));
-    parsed_tokens = parse(tokens);
-    signal_setting();
-    exec_cmd(parsed_tokens, path);
+	path = get_path(getenv("PATH"));
+	parsed_tokens = parse(tokens);
+	signal_setting();
+	exec_cmd(parsed_tokens, path);
 }
