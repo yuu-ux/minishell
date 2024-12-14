987329076f734896930d4c70f38f9bc0e8723fb6
yuu-ux <yuugaehara@gmail.com>
2024-12-14 13:07:41 +0000

    :recycle: 関数が25行以内になるように調整

--- srcs/tokenize.c
+++ srcs/tokenize.c
@@ -10,23 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <tokenize.h>
-#include <libft.h>
 #include <debug.h>
+#include <libft.h>
+#include <tokenize.h>
 
 static size_t	skip_quoted_token(char quote_char, const char **line)
 {
-	size_t moved;
+	size_t	moved;
 
 	moved = 0;
+	(*line)++;
+	moved++;
 	while (**line)
 	{
 		// echo "hello""world"をひとつのトークンとして持ちたいため次がスペースかチェックする
-		if (**line == quote_char && **(line+1) == ' ')
+		if (**line == quote_char && *(*line + 1) == ' ')
 		{
 			(*line)++;
 			moved++;
-			break;
+			break ;
 		}
 		(*line)++;
 		moved++;
@@ -34,7 +36,7 @@ static size_t	skip_quoted_token(char quote_char, const char **line)
 	return (moved);
 }
 
-static size_t	skip_while(int	(*is_skip)(int), const char **line)
+static size_t	skip_while(int (*is_skip)(int), const char **line)
 {
 	size_t	moved;
 
@@ -47,128 +49,132 @@ static size_t	skip_while(int	(*is_skip)(int), const char **line)
 	return (moved);
 }
 
-static size_t  count_tokens(const char *line)
+static size_t	skip_non_delimiter(const char **line)
 {
-    size_t  count;
+	size_t	moved;
 
-    count = 0;
-    while (*line)
-    {
-        if (ft_isspace(*line))
-			skip_while(ft_isspace, &line);
-        else if (is_operators(*line))
-        {
-            count++;
-			skip_while(is_operators, &line);
-        }
-        else if (is_quote(*line))
-        {
-            count++;
-			skip_quoted_token(*(line++), &line);
-        }
-        else
-        {
-            count++;
-            while (!(is_operators(*line) || is_quote(*line) || ft_isspace(*line)) && *line)
-                line++;
-        }
-    }
-    return (count);
-}
-
-
-static char    **tokenize(const char *line)
-{
-    char **tokens = NULL;
-    size_t  word_num;
-    size_t  index;
-    size_t  moved;
-
-    index = 0;
-    word_num = count_tokens(line);
-    tokens = (char **)malloc((word_num + 1) * sizeof(char *));
-	if (!tokens)
-		return (NULL);
 	moved = 0;
-    while (index < word_num)
-    {
+	while (!(is_operators(**line) || is_quote(**line) || ft_isspace(**line))
+		&& **line)
+	{
+		moved++;
+		(*line)++;
+	}
+	return (moved);
+}
+
+static size_t	count_tokens(const char *line)
+{
+	size_t	count;
+
+	count = 0;
+	while (*line)
+	{
+		if (ft_isspace(*line))
+			skip_while(ft_isspace, &line);
+		else if (is_operators(*line))
+		{
+			count++;
+			skip_while(is_operators, &line);
+		}
+		else if (is_quote(*line))
+		{
+			count++;
+			skip_quoted_token(*line, &line);
+		}
+		else
+		{
+			count++;
+			skip_non_delimiter(&line);
+		}
+	}
+	return (count);
+}
+
+static char	**tokenize(const char *line)
+{
+	char	**tokens;
+	size_t	word_num;
+	size_t	index;
+	size_t	moved;
+
+	tokens = NULL;
+	word_num = count_tokens(line);
+	tokens = (char **)ft_xmalloc((word_num + 1) * sizeof(char *));
+	index = 0;
+	while (index < word_num)
+	{
+		moved = 0;
 		skip_while(ft_isspace, &line);
-        if (is_operators(*line))
+		if (is_operators(*line))
 			moved += skip_while(is_operators, &line);
-        else if (is_quote(*line))
-			moved += skip_quoted_token(*(line++), &line);
-        else
-        {
-            while (!(is_operators(*line) || is_quote(*line) || ft_isspace(*line)) && *line)
-            {
-                moved++;
-                line++;
-            }
-        }
-        tokens[index] = (char *)malloc((moved + 1) * sizeof(char));
-        ft_memcpy(tokens[index], line-moved, moved);
-        tokens[index][moved] = '\0';
-        index++;
-    }
-    tokens[index] = NULL;
-    return (tokens);
+		else if (is_quote(*line))
+			moved += skip_quoted_token(*line, &line);
+		else
+			moved += skip_non_delimiter(&line);
+		tokens[index] = (char *)ft_xmalloc((moved + 1) * sizeof(char));
+		ft_memcpy(tokens[index], line - moved, moved);
+		tokens[index][moved] = '\0';
+		index++;
+	}
+	tokens[index] = NULL;
+	return (tokens);
 }
 
-static t_token *new_token(const char *line, t_token_type type)
+static t_token	*new_token(const char *line, t_token_type type)
 {
-    t_token *token;
+	t_token	*token;
 
-    if (!line)
-        return (NULL);
-    token = (t_token *)malloc(sizeof(t_token));
-    if (!token)
-        return (NULL);
-    token->data = ft_strdup(line);
-    token->next = NULL;
-    token->type = type;
-    return (token);
+	if (!line)
+		return (NULL);
+	token = (t_token *)malloc(sizeof(t_token));
+	if (!token)
+		return (NULL);
+	token->data = ft_strdup(line);
+	token->next = NULL;
+	token->type = type;
+	return (token);
 }
 
-static t_token_type    get_token_type(const char *token)
+static t_token_type	get_token_type(const char *token)
 {
-    if (*token == '|')
-        return (TOKEN_PIPE);
-    else if (*token == '<' && *(token+1) == '<')
-        return (TOKEN_REDIRECT_HEREDOC);
-    else if (*token == '>' && *(token+1) == '>')
-        return (TOKEN_REDIRECT_APPEND);
-    else if (*token == '<')
-        return (TOKEN_REDIRECT_IN);
-    else if (*token == '>')
-        return (TOKEN_REDIRECT_OUT);
-    else
-        return (TOKEN_WORD);
+	if (*token == '|')
+		return (TOKEN_PIPE);
+	else if (*token == '<' && *(token + 1) == '<')
+		return (TOKEN_REDIRECT_HEREDOC);
+	else if (*token == '>' && *(token + 1) == '>')
+		return (TOKEN_REDIRECT_APPEND);
+	else if (*token == '<')
+		return (TOKEN_REDIRECT_IN);
+	else if (*token == '>')
+		return (TOKEN_REDIRECT_OUT);
+	else
+		return (TOKEN_WORD);
 }
 
-t_token    *tokenization(const char *line)
+t_token	*tokenization(const char *line)
 {
-    t_token	*token;
-    t_token	*new;
-    char    **tokens;
-    t_token *head = NULL;
-    int i;
+	t_token	*token;
+	t_token	*new;
+	char	**tokens;
+	t_token	*head;
+	int		i;
 
-    i = 0;
+	head = NULL;
+	i = 0;
 	if (line == NULL)
 		return (NULL);
-    tokens = tokenize(line);
-    while (tokens[i])
-    {
-        new = new_token(tokens[i], get_token_type(tokens[i]));
-        if (!head)
-            head = new;
-        else
-            token->next = new;
-        token = new;
-        i++;
-    }
+	tokens = tokenize(line);
+	while (tokens[i])
+	{
+		new = new_token(tokens[i], get_token_type(tokens[i]));
+		if (!head)
+			head = new;
+		else
+			token->next = new;
+		token = new;
+		i++;
+	}
 	token->next = NULL;
-	print_token(head);
-    return (head);
+	return (head);
 }
-
