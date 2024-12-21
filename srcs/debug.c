77cc2be9c69e43db116954ebf9e85c3a3d503226
yuu-ux <yuugaehara@gmail.com>
2024-12-21 06:13:51 +0000

    :wrench: (nil) 表示追加

--- srcs/debug.c
+++ srcs/debug.c
@@ -1,24 +1,34 @@
-#include <tokenize.h>
 #include <invoke_commands.h>
+#include <tokenize.h>
 
-const char *get_token_type_string(t_token_type type)
+const char	*get_token_type_string(t_token_type type)
 {
-    switch (type) {
-        case TOKEN_WORD: return "TOKEN_WORD";
-        case TOKEN_EOF: return "TOKEN_EOF";
-        case TOKEN_UNKNOWN: return "TOKEN_UNKNOWN";
-        case TOKEN_PIPE: return "TOKEN_PIPE";
-        case TOKEN_REDIRECT_IN: return "TOKEN_REDIRECT_IN";
-        case TOKEN_REDIRECT_OUT: return "TOKEN_REDIRECT_OUT";
-        case TOKEN_REDIRECT_APPEND: return "TOKEN_REDIRECT_APPEND";
-        case TOKEN_REDIRECT_HEREDOC: return "TOKEN_REDIRECT_HEREDOC";
-        default: return "UNKNOWN_TYPE";
-    }
+	switch (type)
+	{
+	case TOKEN_WORD:
+		return ("TOKEN_WORD");
+	case TOKEN_EOF:
+		return ("TOKEN_EOF");
+	case TOKEN_UNKNOWN:
+		return ("TOKEN_UNKNOWN");
+	case TOKEN_PIPE:
+		return ("TOKEN_PIPE");
+	case TOKEN_REDIRECT_IN:
+		return ("TOKEN_REDIRECT_IN");
+	case TOKEN_REDIRECT_OUT:
+		return ("TOKEN_REDIRECT_OUT");
+	case TOKEN_REDIRECT_APPEND:
+		return ("TOKEN_REDIRECT_APPEND");
+	case TOKEN_REDIRECT_HEREDOC:
+		return ("TOKEN_REDIRECT_HEREDOC");
+	default:
+		return ("UNKNOWN_TYPE");
+	}
 }
 
 void	print_token(t_token *head)
 {
-	t_token *current;
+	t_token	*current;
 
 	current = head;
 	while (current)
@@ -30,22 +40,24 @@ void	print_token(t_token *head)
 
 void	print_node(t_node *head)
 {
-	t_node *current;
-	int	i;
+	t_node	*current;
+	int		i;
 
-    current = head;
-    while (current)
-    {
-        printf("kind:%d\n", current->kind);
-        if (current->argv != NULL)
-        {
+	current = head;
+	while (current)
+	{
+		printf("kind:%d\n", current->kind);
+		if (current->argv != NULL)
+		{
 			i = 0;
-            while (current->argv[i])
-                printf("argv:%s\n", current->argv[i++]);
-        }
-        printf("in:%d\n", current->fd_in);
-        printf("out:%d\n", current->fd_out);
-        printf("---------------------------------\n");
-        current = current->next;
-    }
+			while (current->argv[i])
+				printf("argv:%s\n", current->argv[i++]);
+		}
+		else
+			printf("argv:(nil)\n");
+		printf("in:%d\n", current->fd_in);
+		printf("out:%d\n", current->fd_out);
+		printf("---------------------------------\n");
+		current = current->next;
+	}
 }
