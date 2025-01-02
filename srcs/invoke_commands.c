ac8a415bf71999560e27539ec9d877e0fcd80e6a
yuu-ux <yuugaehara@gmail.com>
2025-01-02 15:25:48 +0900

    :bug: hoge/test.sh のようなケースを実行できるように修正

--- srcs/invoke_commands.c
+++ srcs/invoke_commands.c
@@ -3,80 +3,80 @@
 
 static int	exec_single_cmd(const t_node *parsed_tokens, char **path_list)
 {
-    pid_t	pid;
+	pid_t	pid;
 
 	pid = fork();
 	if (pid == -1)
 		perror("error\n");
 	if (pid == 0)
 	{
-		if (parsed_tokens->argv[0][0] == '/')
-		{
-			if (access(parsed_tokens->argv[0], F_OK) == 0)
-				execve(parsed_tokens->argv[0], parsed_tokens->argv, NULL);
-		}
+		if (access(parsed_tokens->argv[0], F_OK) == 0)
+			execve(parsed_tokens->argv[0], parsed_tokens->argv, NULL);
 		else
-            execve(find_executable_path(parsed_tokens, path_list), parsed_tokens->argv, NULL);
+			execve(find_executable_path(parsed_tokens, path_list),
+				parsed_tokens->argv, NULL);
 	}
 	else
 		waitpid(pid, NULL, 0);
 	return (EXIT_FAILURE);
 }
 
-int exec_pipe(t_node *parsed_tokens, t_exe_info *info, char **path_list)
+int	exec_pipe(t_node *parsed_tokens, t_exe_info *info, char **path_list)
 {
-    pipe(parsed_tokens->fds);
-    info->pid[info->exec_count] = fork();
-    if (info->pid[info->exec_count] == -1)
-        return (EXIT_FAILURE);
-    if (info->pid[info->exec_count] == 0)
-        child_process(parsed_tokens, info, path_list);
-   parent_process(parsed_tokens, info);
-    info->exec_count++;
-   return (EXIT_SUCCESS);
+	pipe(parsed_tokens->fds);
+	info->pid[info->exec_count] = fork();
+	if (info->pid[info->exec_count] == -1)
+		return (EXIT_FAILURE);
+	if (info->pid[info->exec_count] == 0)
+		child_process(parsed_tokens, info, path_list);
+	parent_process(parsed_tokens, info);
+	info->exec_count++;
+	return (EXIT_SUCCESS);
 }
 
-int exec_last_pipe_cmd(t_node *parsed_tokens, t_exe_info *info, char **path_list)
+int	exec_last_pipe_cmd(t_node *parsed_tokens, t_exe_info *info,
+		char **path_list)
 {
-    info->pid[info->exec_count] = fork();
-    if (info->pid[info->exec_count] == -1)
-        return (ft_printf("error\n"), EXIT_FAILURE);
-    if (info->pid[info->exec_count] == 0)
-    {
-        wrap_dup2(info->before_cmd_fd, STDIN_FILENO);
-        wrap_close(info->before_cmd_fd);
-        execute(parsed_tokens, path_list);
-        exit(EXIT_FAILURE);
-    }
-    wrap_close(info->before_cmd_fd);
-    while (info->exec_count >= 0)
-    {
-        waitpid(info->pid[info->exec_count], NULL, 0);
-        info->exec_count--;
-    }
-    return (EXIT_SUCCESS);
+	info->pid[info->exec_count] = fork();
+	if (info->pid[info->exec_count] == -1)
+		return (ft_printf("error\n"), EXIT_FAILURE);
+	if (info->pid[info->exec_count] == 0)
+	{
+		wrap_dup2(info->before_cmd_fd, STDIN_FILENO);
+		wrap_close(info->before_cmd_fd);
+		execute(parsed_tokens, path_list);
+		exit(EXIT_FAILURE);
+	}
+	wrap_close(info->before_cmd_fd);
+	while (info->exec_count >= 0)
+	{
+		waitpid(info->pid[info->exec_count], NULL, 0);
+		info->exec_count--;
+	}
+	return (EXIT_SUCCESS);
 }
 
 int	exec_cmd(t_node *parsed_tokens, char **path_list)
 {
-    t_exe_info *info;
+	t_exe_info	*info;
 
-    if (parsed_tokens->next == NULL && parsed_tokens->argv != NULL)
-        return (exec_single_cmd(parsed_tokens, path_list));
-    info = (t_exe_info *)malloc(sizeof(t_exe_info));
-    if (info == NULL)
-        exit(EXIT_FAILURE);
-    if (initialize_info(info, parsed_tokens))
-        return (EXIT_FAILURE);
-    while (parsed_tokens->next)
-    {
-        if (parsed_tokens->kind == CMD)
-        {
-            if (exec_pipe(parsed_tokens, info, path_list) == 1)
-                return (EXIT_FAILURE);
-        }
-        parsed_tokens = parsed_tokens->next;
-    }
+	// TODO hoge/test.sh ようなケースを実行できるようにする
+	if (parsed_tokens->next == NULL && parsed_tokens->argv != NULL)
+		return (exec_single_cmd(parsed_tokens, path_list));
+	info = (t_exe_info *)malloc(sizeof(t_exe_info));
+	if (info == NULL)
+		exit(EXIT_FAILURE);
+	if (initialize_info(info, parsed_tokens))
+		return (EXIT_FAILURE);
+	while (parsed_tokens->next)
+	{
+		if (parsed_tokens->kind == CMD)
+		{
+			if (exec_pipe(parsed_tokens, info, path_list) == 1)
+				return (EXIT_FAILURE);
+		}
+		parsed_tokens = parsed_tokens->next;
+	}
 	return (exec_last_pipe_cmd(parsed_tokens, info, path_list));
 }
 
