39b5d71d9cbae02dfe4cda22691f8be8ce612f42
yuu-ux <yuugaehara@gmail.com>
2025-02-26 21:12:05 +0900

    :wrench: ? を展開する処理を追加

--- includes/units/expand.h
+++ includes/units/expand.h
@@ -24,8 +24,7 @@ t_token *expand_tokens(t_token **_tokens, t_context *context);
 
 // expand_utils
 char	*search_env(const char *key, t_kvs *environ);
-size_t	count_key_len(char *token, int t_index);
-size_t	insert_env(char **buffer, char *token, t_kvs *environ);
+size_t	insert_env(char **buffer, char *token, t_context *context);
 size_t	expand_variable(char **result, char *token, t_context *context,
 		int start, int i);
 
